bc6585bd6e882c38a00ffab4498d762f893d383d
Emanuel Vigil <emagaston12@gmail.com>
2024-06-28 22:38:24 -0300

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Formulario from './components/Formulario/formulario';
 import MiOrg from './components/MiOrg';
 import Equipo from './components/Equipo';
 import Footer from './components/Footer';
-import Colaborador from './components/Colaborador';
 
 function App() {
 
@@ -15,43 +14,6 @@ function App() {
     //obtiene los valores del localstorage
     const savedColaboradores = localStorage.getItem('colaboradores');
     return savedColaboradores ? JSON.parse(savedColaboradores) : [];})
-
-  // const [equipos, actualizarEquipos] = useState([
-  //   {
-  //     titulo:"Programación",
-  //     colorPrimario: "#57C278",
-  //     colorSecundario: "#D9F7E9"
-  //   },
-  //   {
-  //     titulo:"Front End",
-  //     colorPrimario: "#82CFFA",
-  //     colorSecundario: "#E8F8FF"
-  //   },
-  //   {
-  //     titulo:"Data Science",
-  //     colorPrimario: "#A6D157",
-  //     colorSecundario: "#F0F8E2"
-  //   },
-  //   {
-  //     titulo:"Devops",
-  //     colorPrimario: "#E06B69",
-  //     colorSecundario: "#FDE7E8"
-  //   },
-  //   {
-  //     titulo:"UX y Diseño",
-  //     colorPrimario: "#DB6EBF",
-  //     colorSecundario: "#FAE9F5"
-  //   },
-  //   {
-  //     titulo:"Móvil",
-  //     colorPrimario: "#FFBA05",
-  //     colorSecundario: "#FFF5D9"
-  //   },
-  //   {
-  //     titulo:"Innovacion y gestion",
-  //     colorPrimario: "#FF8A29",
-  //     colorSecundario: "#FFEEDF"
-  //   }])
 
   const initialEquipos = [
     { id: uuidv4(), titulo: "Programación", colorPrimario: "#57C278", colorSecundario: "#D9F7E9", fav: false},
@@ -73,16 +35,6 @@ function App() {
     setMostrarForm(!mostrarForm)
   }
 
-  // const registrarColaborador = (colaborador) =>{
-  //   //Spread operator copia valores de una lista
-  //   colaborador.id = uuidv4();
-  //   const nuevosColaboradores = [...colaboradores, colaborador]
-  //   console.log(nuevosColaboradores)
-  //   actualizarColaboradores(nuevosColaboradores)
-  //   //guarda los valores del localstorage
-  //   localStorage.setItem('colaboradores',JSON.stringify(nuevosColaboradores))
-  // }
-
   const registrarColaborador = (colaborador) => {
     colaborador.id = uuidv4();
     // Buscar el equipo correspondiente en initialEquipos
@@ -99,7 +51,6 @@ function App() {
       (colaborador) => colaborador.id !== id);
     actualizarColaboradores(colaboradoresActualizados);
     localStorage.setItem('colaboradores', JSON.stringify(colaboradoresActualizados));
-    
   }
 
   const actualizarColor = (color, id) =>{
@@ -130,12 +81,10 @@ function App() {
   return (
     <div>
       <Header />
-      {/*mostrarForm ? <Formulario /> : <></>*/}
       {mostrarForm && <Formulario 
       equipos = {equipos.map((equipo) => equipo.titulo)} 
       registrarColaborador={registrarColaborador} 
       crearEquipo={crearEquipo}/>}
-
       <MiOrg cambiarMostrar={cambiarMostrar}/>
       {equipos.map( (equipo) =><Equipo 
       datos={equipo} 
@@ -145,11 +94,8 @@ function App() {
       actualizarColor={actualizarColor}
       like = {like}
       />)}
-
       <Footer />
     </div>  
   );
 }
-
-
 export default App;
